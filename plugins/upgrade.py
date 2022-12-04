952846dd134384484a099ad7fefbba0d8e172b2e
haashim0 <103762873+haashim0@users.noreply.github.com>
2022-12-04 15:58:49 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -34,7 +34,7 @@ async def upgrade(bot,update):
 
 @Client.on_message(filters.private & filters.command(["upgrade"]))
 async def upgradecm(bot,message):
-	text = """**Free Plan User**
+	text = """<b>**Free Plan User**
 	Daily  Upload limit 2GB
 	Price 0
 	
@@ -51,12 +51,11 @@ async def upgradecm(bot,message):
 	Price Rs 150  🇮🇳/🌎 1.81$  per Month
 	
 	
-	Pay Using Upi I'd ```lokamandc1224@oksbi```
+	Pay Using Upi I'd ```655365838@paytm```
 	
 	After Payment Send Screenshots Of 
-        Payment To Admin"""
+        Payment To Admin</b>"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/mrlokaman")], 
-        			[InlineKeyboardButton("PayPal 🌎",url = "https://www.paypal.me/lokamanchendekar"),
-        			InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/los89jy0")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+        			InlineKeyboardButton("ADMIN 🙋",url = "https://t.me/haashim_999")], 
+        			[InlineKeyboardButton("Paytm💰",url = "https://p.paytm.me/xCTH/dtvlquzy")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
 	await message.reply_text(text = text,reply_markup = keybord)
