33709e5d77e93c2fec31e9d6e726b3897f15dff2
haashim0 <103762873+haashim0@users.noreply.github.com>
2022-12-04 17:23:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,23 +4,13 @@ import os
 
 from plugins.cb_data import app as Client2
 
-TOKEN = os.environ.get("TOKEN", "5945219564:AAHMjrNCDXTgfBT-HuBmxjDN_0RHOORYqbA")
+TOKEN = os.environ.get("TOKEN", "")
 
-ADMIN = os.environ.get("ADMIN", "5291606032")
+API_ID = int(os.environ.get("API_ID", ""))
 
-API_ID = int(os.environ.get("API_ID", "14505719"))
-
-API_HASH = os.environ.get("API_HASH", "620f0a2aa2cd1474a4953619b3e3643d")
+API_HASH = os.environ.get("API_HASH", "")
 
 STRING = os.environ.get("STRING", "")
-
-LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001855524980")
-
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://Haashim:Haashim@mfile0.t9hxg.mongodb.net/?retryWrites=true&w=majority")
-
-DB_NAME = os.environ.get("DB_NAME", "re1")
-
-CHANNEL = os.environ.get("CHANNEL", "@tamildub_linkzz")
 
 
 bot = Client(
