13c9966f6489bfd9175a161618212320d010a069
haashim0 <103762873+haashim0@users.noreply.github.com>
2022-12-06 19:28:32 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -1,7 +1,6 @@
 import os 
-from info import TOKEN
 from pyrogram import Client, filters
-token = os.environ.get('TOKEN','')
+token = os.environ.get('TOKEN','5945219564:AAHNUQJuBMGsjQgzXg-p_wB-FrHhO9PqfMQ')
 botid = token.split(':')[0]
 from helper.database import botdata, find_one, total_user
 from helper.progress import humanbytes
