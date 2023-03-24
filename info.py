ac4614d5cc2f5f9b6591717933439fb400417056
bu2terFLY <119470972+bu2terFLY@users.noreply.github.com>
2023-03-24 11:48:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -4,11 +4,11 @@ import os
 
 from plugins.cb_data import app as Client2
 
-TOKEN = os.environ.get("TOKEN", "5945219564:AAHNUQJuBMGsjQgzXg-p_wB-FrHhO9PqfMQ")
+TOKEN = os.environ.get("TOKEN", "5759212584:AAHt_BtGwB9xaORga9hX8jb0zUl50xoPAyQ")
 
-API_ID = int(os.environ.get("API_ID", "14505719"))
+API_ID = int(os.environ.get("API_ID", "23223511"))
 
-API_HASH = os.environ.get("API_HASH", "620f0a2aa2cd1474a4953619b3e3643d")
+API_HASH = os.environ.get("API_HASH", "c2207a11155ad050097e981fdd5fd0b1")
 
-STRING = os.environ.get("STRING", "")
+STRING = os.environ.get("STRING", "BQCvL9oONeonVdwPOyfuAQSJcrfYg0whrRVpfm8qgIsvFzm124fasi_2Mkh7ul--Gu-RVgqnc9htb7qQfjZ7_OuAZR7cdNPvLYr6nRWSQFu7_paoOhxEOyWE987iBUtegfkAvc4l-yByvOAY4PaF2enmjPNFvW4d55R2nWNbPFHpRt8jneSrKDT_NbmYW1mkEqfNhfk6K8PR6iCCT417JyNb3RHMsGObzMmsCGOXWAtEe16dg4T1TdzXjKfQSrp-aPHoH4BlBCmzG7O_y8UMzeH4QXBoitZUOULs1A46xEhHnjUjdgG3DISgAe3KHOJMeIR9HUKLgWQ3NFNOfMwWhtq-AAAAAS0MIIwA")
 
